7ad39d2ac7a5264b201f86bf56c436ba25dae420
let-value <let.value@gmail.com>
2022-03-27 21:57:32 +0300

    db

---
+++ src/data-source.ts
@@ -0,0 +1,11 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { Track } from "./entity/Track";
+
+export const AppDataSource = new DataSource({
+    type: "sqlite",
+    database: ":memory:",
+    synchronize: true,
+    logging: true,
+    entities: [Track]
+});
---
+++ src/entity/Track.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Track {
+    @PrimaryGeneratedColumn()
+    Id: number;
+
+    @Column()
+    Name: string;
+
+    @Column()
+    Artist: string;
+
+    @Column()
+    Album: string;
+
+    @Column()
+    Playlist: string;
+
+    @Column()
+    Type: string;
+
+    @Column()
+    ISRC: string;
+}
